df8033f980d2296bb98ebb90391f8688220d13fe
Abhishek Sharma <abhish3kk@gmail.com>
2025-02-15 11:31:31 +0530

    feat: integrate github api #9

--- src/common/utils/envConfig.ts
+++ src/common/utils/envConfig.ts
@@ -11,4 +11,10 @@ export const env = cleanEnv(process.env, {
   COMMON_RATE_LIMIT_MAX_REQUESTS: num({ devDefault: testOnly(1000) }),
   COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
   SECRET_KEY: str({ devDefault: crypto.randomUUID() }),
+  PAT_TOKEN_GITHUB: str({ devDefault: crypto.randomUUID() }),
 });
+
+export const endpoints = {
+  GITHUB_SEARCH_REPOS: "https://api.github.com/search/repositories",
+  GITHUB_STARRED_REPOS: "https://api.github.com/user/starred",
+};
