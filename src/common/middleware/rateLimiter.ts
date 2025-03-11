ad1f6c929fa6f9bcab68361ddd008a1560550bd5
Abhishek Sharma <abhish3kk@gmail.com>
2025-03-11 18:52:58 +0530

    improve ratelimiter message

--- src/common/middleware/rateLimiter.ts
+++ src/common/middleware/rateLimiter.ts
@@ -6,7 +6,10 @@ import { env } from "@/common/utils/envConfig";
 const rateLimiter = rateLimit({
   legacyHeaders: true,
   limit: env.COMMON_RATE_LIMIT_MAX_REQUESTS,
-  message: "Too many requests, please try again later.",
+  message: {
+    error: "Too many requests, please try again later.",
+    retryAfter: `${Math.ceil((15 * 60 * env.COMMON_RATE_LIMIT_WINDOW_MS) / 1000 / 60)} + minutes`,
+  },
   standardHeaders: true,
   windowMs: 15 * 60 * env.COMMON_RATE_LIMIT_WINDOW_MS,
   keyGenerator: (req: Request) => req.ip as string,
